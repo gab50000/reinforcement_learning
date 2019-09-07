8392b8202b82dc61fab5358ff80e896f5eb4650b
Gabriel Kabbe <gabriel.kabbe@mail.de>
2019-09-07 10:20:22 +0200

    Add training loop

--- main.py
+++ main.py
@@ -7,21 +7,40 @@ def run(model, render=True):
     env = gym.make("CartPole-v0")
     env.reset()
     action = env.action_space.sample()
+
+    states, actions, rewards = [], [], []
+    observation, _, done, info = env.step(action)
+
     for _ in range(1000):
+        if done:
+            break
+
         if render:
             env.render()
+
+        states.append(observation)
+
+        model_out = model(torch.from_numpy(observation).type(torch.float32))
+        prob = model_out.detach().numpy().astype(float)
+        prob /= prob.sum()
+        action = np.random.choice(2, p=prob)
+
         observation, reward, done, info = env.step(action)
-        model_out = (
-            model(torch.from_numpy(observation).type(torch.float32)).detach().numpy()
-        )
-        action = np.argmax(model_out)
+
+        actions.append(action)
+        rewards.append(reward)
 
     env.close()
+
+    return states, actions, rewards
 
 
 def main():
     model = torch.nn.Sequential(
-        torch.nn.Linear(4, 10), torch.nn.ReLU(), torch.nn.Linear(10, 2)
+        torch.nn.Linear(4, 10),
+        torch.nn.ReLU(),
+        torch.nn.Linear(10, 2),
+        torch.nn.Sigmoid(),
     )
     run(model)
 
