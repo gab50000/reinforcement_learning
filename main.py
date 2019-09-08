7e59a1e33a3f724f5b7b7e801c908f357aaee3af
Gabriel Kabbe <gabriel.kabbe@mail.de>
2019-09-08 14:50:37 +0200

    First working version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import torch
+import fire
 import gym
 import numpy as np
+import torch
 
 
 def run(model, render=True):
@@ -11,8 +12,9 @@ def run(model, render=True):
     states, action_probs, actions, rewards = [], [], [], []
     observation, _, done, info = env.step(action)
 
-    for _ in range(1000):
+    for n in range(1000):
         if done:
+            rewards[-1] = -10
             break
 
         if render:
@@ -30,18 +32,27 @@ def run(model, render=True):
         action_probs.append(model_out)
         actions.append(action)
         rewards.append(reward)
+    else:
+        # no game over -> big reward
+        rewards[-1] = 10
+
+    print(f"Stop after {n} epochs")
 
     env.close()
 
     return states, action_probs, actions, rewards
 
 
-def normalize_rewards(rewards, discount_factor=0.9):
+def normalize_rewards(rewards, discount_factor=0.9, normalize=False):
     discounted = np.zeros(len(rewards))
     discounted[-1] = rewards[-1]
 
     for i in reversed(range(discounted.shape[0] - 1)):
         discounted[i] = rewards[i] + discount_factor * discounted[i + 1]
+
+    if normalize:
+        discounted = (discounted - discounted.mean()) / discounted.std()
+
     return discounted
 
 
@@ -52,30 +63,36 @@ def test_normalize_rewards():
     np.testing.assert_allclose(target, result)
 
 
-def evaluate(states, action_probs, actions, rewards, model):
-    for s, ap, ac, r in zip(states, action_probs, actions, rewards):
-        discounted_rewards = normalize_rewards(rewards)
-        # check if the action in response to a given state led to a positive or
-        # a negative reward
-        # if yes, enforce this action
-        prob_selection = torch.stack(action_probs)[range(len(action_probs)), actions]
-        prob_selection.backward()
+def evaluate(states, action_probs, actions, rewards, model, learning_rate=0.1):
+    discounted_rewards = normalize_rewards(rewards, normalize=True)
+    # check if the action in response to a given state led to a positive or
+    # a negative reward
+    # if yes, enforce this action
+    prob_selection = torch.stack(action_probs)[range(len(action_probs)), actions]
+    prob_reward = (
+        prob_selection * torch.from_numpy(discounted_rewards).type(torch.float32)
+    ).sum()
 
-        with torch.no_grad():
-            for param in model.parameters():
-                param += ...
+    prob_reward.backward()
+    with torch.no_grad():
+        for param in model.parameters():
+            param += learning_rate * param.grad
+            param.grad.zero_()
 
 
-def main():
+def main(n_epochs=100, learning_rate=0.1):
     model = torch.nn.Sequential(
         torch.nn.Linear(4, 10),
         torch.nn.ReLU(),
         torch.nn.Linear(10, 2),
         torch.nn.Sigmoid(),
     )
-    states, action_probs, actions, rewards = run(model)
-    evaluate(states, action_probs, actions, rewards, model)
+    for _ in range(n_epochs):
+        states, action_probs, actions, rewards = run(model)
+        evaluate(
+            states, action_probs, actions, rewards, model, learning_rate=learning_rate
+        )
 
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(main)
