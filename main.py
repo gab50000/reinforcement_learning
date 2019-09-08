ecdea2354d5a9436392924578beb74ec037125ac
Gabriel Kabbe <gabriel.kabbe@mail.de>
2019-09-08 13:53:21 +0200

    Update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def run(model, render=True):
     env.reset()
     action = env.action_space.sample()
 
-    states, actions, rewards = [], [], []
+    states, action_probs, actions, rewards = [], [], [], []
     observation, _, done, info = env.step(action)
 
     for _ in range(1000):
@@ -27,12 +27,43 @@ def run(model, render=True):
 
         observation, reward, done, info = env.step(action)
 
+        action_probs.append(model_out)
         actions.append(action)
         rewards.append(reward)
 
     env.close()
 
-    return states, actions, rewards
+    return states, action_probs, actions, rewards
+
+
+def normalize_rewards(rewards, discount_factor=0.9):
+    discounted = np.zeros(len(rewards))
+    discounted[-1] = rewards[-1]
+
+    for i in reversed(range(discounted.shape[0] - 1)):
+        discounted[i] = rewards[i] + discount_factor * discounted[i + 1]
+    return discounted
+
+
+def test_normalize_rewards():
+    rewards = [1, 1, 1, 10, 1, -10]
+    target = [4.7512, 4.168, 3.52, 2.8, -8, -10]
+    result = normalize_rewards(rewards)
+    np.testing.assert_allclose(target, result)
+
+
+def evaluate(states, action_probs, actions, rewards, model):
+    for s, ap, ac, r in zip(states, action_probs, actions, rewards):
+        discounted_rewards = normalize_rewards(rewards)
+        # check if the action in response to a given state led to a positive or
+        # a negative reward
+        # if yes, enforce this action
+        prob_selection = torch.stack(action_probs)[range(len(action_probs)), actions]
+        prob_selection.backward()
+
+        with torch.no_grad():
+            for param in model.parameters():
+                param += ...
 
 
 def main():
@@ -42,7 +73,8 @@ def main():
         torch.nn.Linear(10, 2),
         torch.nn.Sigmoid(),
     )
-    run(model)
+    states, action_probs, actions, rewards = run(model)
+    evaluate(states, action_probs, actions, rewards, model)
 
 
 if __name__ == "__main__":
